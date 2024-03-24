27df8c2c18f804556237af5d7f83e1ad44c0dde1
manjula808@gmail.com <manjula808@gmail.com>
2024-03-24 20:09:36 +0530

    Changing for Grocery app

--- reading-list-front-end-with-managed-auth/src/components/modal/fragments/add-item.tsx
+++ reading-list-front-end-with-managed-auth/src/components/modal/fragments/add-item.tsx
@@ -57,31 +57,31 @@ export default function AddItem(props: AddItemProps) {
       <form className="bg-white rounded pt-2 pb-1">
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2">
-            Title
+            Item Name
           </label>
           <input
             className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="name"
             type="text"
-            placeholder="Name"
+            placeholder="Item Name"
             onChange={(e) => setName(e.target.value)}
           />
         </div>
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2">
-            Author
+            Quantity
           </label>
           <input
             className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="author"
             type="text"
-            placeholder="Author"
+            placeholder="Quantity"
             onChange={(e) => setAuthor(e.target.value)}
           />
         </div>
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2">
-            Status
+            Urgency
           </label>
           <Listbox value={status} onChange={setStatus}>
             <div className="relative mt-1">
@@ -134,7 +134,7 @@ export default function AddItem(props: AddItemProps) {
     <Modal
       isOpen={isOpen}
       setIsOpen={setIsOpen}
-      title="Add Book"
+      title="Add Item"
       children={innerFragment}
       handleSubmit={handleOnSubmit}
       isDisabled={!(name && author)}
--- reading-list-front-end/src/index.tsx
+++ reading-list-front-end/src/index.tsx
@@ -165,7 +165,7 @@ export default function App() {
         <div className="container px-4 mx-auto flex justify-center">
           <div className="w-full max-w-lg px-2 py-16 sm:px-0 mb-20">
             <div className="flex justify-between">
-              <p className="text-4xl text-white mb-3 font-bold">Reading List</p>
+              <p className="text-4xl text-white mb-3 font-bold">Grocery List</p>
               <div className="container w-auto">
                 <button
                   className="float-right bg-black bg-opacity-20 p-2 rounded-md text-sm my-3 font-medium text-white h-10"
